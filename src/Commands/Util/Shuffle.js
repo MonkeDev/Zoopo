73fd319f281db89df5b59fe1cdaf994775b9a88f
Mafia#7777 <67177667+Mafia-7777@users.noreply.github.com>
2021-02-07 21:46:03 -0500

    Create Shuffle.js

---
+++ src/Commands/Util/Shuffle.js
@@ -0,0 +1,22 @@
+const Base = require('../../BaseCommand');
+const fetch = require('node-fetch').default;
+module.exports = class Help extends Base {
+    constructor(bot) {
+        super(bot, {
+            name: 'shuffle',
+            desc: 'shuffle some text',
+            usage: 'shuffle <text>'
+        });
+    };
+
+    async run(msg, args, data){
+
+
+        const res = await (await (fetch(`${this.bot.baseApiUrl}/utils/shuffle?key=${this.bot.config.apiKey}&content=${encodeURI(args.join(' ') || 'What would you like to shuffle?')}`))).json();
+        
+        msg.channel.createMessage(res.result);
+
+
+        
+    }
+};
