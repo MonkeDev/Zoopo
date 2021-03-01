fa867a1622f1d2f28da6b327ba2603cf26df6eec
Mafia#7777 <67177667+Mafia-7777@users.noreply.github.com>
2021-03-01 03:58:48 -0500

    Update Zoopo.js

--- Zoopo.js
+++ Zoopo.js
@@ -11,3 +11,10 @@ const Init = async () => {
 };
 
 Init(); 
+
+// Up time thingeeee
+const http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('ok');
+}).listen(25569);
