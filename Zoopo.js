6f5f4303cd865f7f9e07a765a2ffcd1e6813ce79
Mafia#7777 <67177667+Mafia-7777@users.noreply.github.com>
2021-03-04 07:06:28 -0500

    Top.gg things

--- Zoopo.js
+++ Zoopo.js
@@ -13,8 +13,55 @@ const Init = async () => {
 Init(); 
 
 // Up time thingeeee
+/*
 const http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('ok');
 }).listen(25569);
+*/
+
+const app = require('express')();
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch').default;
+
+// Uptime
+app.get('/', (req, res) => {
+    res.status(200).send('ok');
+});
+
+// Votes
+app.post('/vote', bodyParser.json(), (req, res) => {
+    console.log(req.headers, req.body);
+    const { authorization } = req.headers;
+    const { bot, user } = req.body;
+    if(authorization != Config['top.gg_auth']) {
+      console.log(`No auth from ${req.headers['user-agent']}, auth: ${authorization}`);
+      return res.status(400).send('Wrong auth :sob:');
+    };
+    const howMany = Math.floor(Math.random() * 3000);
+    Zoopo.createMessage('779441136719757323', `Wassup <@!${user}>, Thank you for voting for Zoopo! You earned ${howMany} points!`);
+    console.log(user)
+    // 
+
+    fetch('https://afk.monkedev.com/points/add', { method: 'POST', headers: { userid: user, howmany: howMany, auth: Config.adminKey }}).then(res => res.text()).then(console.log)
+});
+
+const port = process.env.PORT || 25569;
+app.listen(port, () => console.log('On port: ' + port));
+
+// Server count 
+setInterval(() => {
+  console.log(Zoopo.guilds.size, Zoopo.shards.size)
+  fetch('https://top.gg/api/bots/807048838362955798/stats', {
+    method: 'POST',
+    body: JSON.stringify({
+      server_count: Zoopo.guilds.size,
+      shard_count: Zoopo.shards.size
+    }),
+    headers: {
+      Authorization: Config['top.gg_token'],
+      'Content-Type': 'application/json'
+    }
+  });
+}, 10000); // 1 min
